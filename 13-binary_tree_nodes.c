729f38bf8d441cdaf975ce650801363306069aca
GODSPE1 <godspy18@gmail.com>
2024-02-29 11:56:18 +0100

    correct documentation error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 /**
  * binary_tree_nodes -  Counts the nodes with at least 1 child in a binary tree
- * @tree: tree is a pointer to the root node of the tree/*
+ * @tree: tree is a pointer to the root node of the tree
  * to count the number of nodes
  *
  * Return: If tree is NULL, the function must return 0.
