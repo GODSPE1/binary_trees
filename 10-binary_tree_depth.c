e3ddcfd583e07b7fa18a1ded0a3fc5c6fba3d8d6
GODSPE1 <godspy18@gmail.com>
2024-02-29 10:16:18 +0100

    correct documentation error

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth - A function that measures/*
+ * binary_tree_depth - A function that measures
  * the depth of a node in a binary tree
  * @tree: A pointer to the node to measure the depth
  *
- * Return: the depth
+ * Return: If tree is NULL, your function must return 0.
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
