f4e3fbc6efdb82d77f2577eb2ff22a05d334cfd2
GODSPE1 <godspy18@gmail.com>
2024-02-29 07:55:11 +0100

    correct logic in condition

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,17 +1,22 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - A function that goes through a binary/*
- * tree using pre-order traversal
+ * binary_tree_preorder - A function that goes through a binary
+ * tree using pre-order traveal
  * @tree: A pointer to the root node of the tree to traverse
- * @func: is a pointer to a function to call for each node./*
+ * @func: is a pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree != NULL || func != NULL)
+	if (tree == NULL || func == NULL)
 	{
-		func(tree->n);
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		return;
 	}
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+
+	binary_tree_preorder(tree->right, func);
+
 }
