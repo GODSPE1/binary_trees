ee91335e07fed90c225d8dbc76c4d4becf76703e
GODSPE1 <godspy18@gmail.com>
2024-02-29 08:20:33 +0100

    correct comments error

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - A function that goes through a binary tree/*
+ * binary_tree_postorder - A function that goes through a binary tree
  * using post-order traversal
  * @tree: A pointer to the root node of the tree to traverse.
- *
- * @func: A pointer to a function to call for each node./*
+ * @func: A pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
  *
+ * Return: If tree or func is NULL, do nothing.
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
