8cbb2003658272a24409f3474e33cf6884375d50
GODSPE1 <godspy18@gmail.com>
2024-03-01 02:35:11 +0100

    update

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,14 +7,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		leaves += 1;
 
-	size_t leaves_left += binary_tree_leaves(tree->left);
-	size_t leaves_right += binary_tree_leaves(tree->right);
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
 
-	return (leaves_left + leaves_right);
+	return (leaves);
 }
